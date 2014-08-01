c304afca26b3a70e7f80394dd0727d7a9df2fcaa
Harish Kumar <hkumarmk@gmail.com>
2014-08-02 04:47:58 +0530

    Added tests on creating file mirror.list and file content test with sample parameters

--- spec/classes/mirror_spec.rb
+++ spec/classes/mirror_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
 describe 'apt::mirror', :type => :class do
   let(:file_mirror) { '/etc/apt/mirror.list' }
-  let(:facts) { { :lsbdistid => 'Debian'  } }
-
+  let(:facts) { { :lsbdistid => 'Debian', :concat_basedir => '/tmp'  } }
   it { should contain_package("apt-mirror").with_ensure('present') }
+
+  it {
+    should create_concat("/etc/apt/mirror.list").with({
+      'owner'   => 'root',
+      'group'   => 'root',
+      'mode'    => '0644',
+    })
+  }
+
+  let(:params) { { 
+    'base_path' => '/apt-mirror',
+    'defaultarch' => 'amd64',
+    'nthreads' => 10,
+    'name' => 'testname',
+  } }
+
+  it {
+    should contain_concat__fragment('apt-mirror-testname.conf').with({
+      :target => '/etc/apt/mirror.list',
+  }) }
+
+  it {
+    should contain_concat__fragment('apt-mirror-testname.conf').with_content("#### Managed by puppet, all local changes will be reverted ###\n############# config ##################\nset base_path         /apt-mirror\nset mirror_path       $base_path/mirror\nset skel_path         $base_path/skel\nset var_path          $base_path/var\nset cleanscript       $base_path/clean.sh\nset defaultarch       amd64\nset postmirror_script $var_path/postmirror.sh\nset run_postmirror    0\nset nthreads          10\nset _tilde            0\n############# end config ##############\n\n")
+#/#### Managed by puppet, all local changes will be reverted ###.*############# config ##################.*/)
+#set[\s\t]+base_path[\s\t]+/apt-mirror.*set[\s\t]+defaultarch\s+amd64.*set[\s\t]+nthreads[\s\t]+10.*############# end config ##############.*/ )
+  }
+
 end
