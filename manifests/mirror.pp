5b16f714adc78aad7c41ce32f2498f1f2e8cb7d6
Harish Kumar <hkumarmk@gmail.com>
2014-08-02 01:11:09 +0530

    Added basic class with apt-mirror package install.

---
+++ manifests/mirror.pp
@@ -0,0 +1,20 @@
+class apt::mirror(
+    $base_path         = '/var/spool/apt-mirror',
+    $mirror_path       = '$base_path/mirror',
+    $skel_path         = '$base_path/skel',
+    $var_path          = '$base_path/var',
+    $cleanscript       = '$base_path/clean.sh',
+    $postmirror_script = '$var_path/postmirror.sh',
+    $defaultarch       = 'amd64',
+    $run_postmirror    = '0',
+    $nthreads          = '20',
+    $_tilde            = '0',
+  ) {
+
+  ## Install apt-mirror package
+  package { 'apt-mirror':
+    ensure => installed,
+  }
+
+}
+
